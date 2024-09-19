3beffe2db0bb43e5c17403b8ac1cceb0757b8d86
alsoshubham <shubhamsahu100100@hotmail.com>
2024-09-19 14:12:26 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-undef */
 import { Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import contacts from './components/contact';
+import projects from './components/projects';
+import about from './components/about';
 
 function App() {
   return (
     <div className="App">
      <Routes>
-      <Route path="/" element={<div>home</div>}/>
-      <Route path="/contact" element={<div>contact us</div>}/>
-      <Route path="/projects" element={<div>projects</div>}/>
-      <Route path="/about" element={<div>about me</div>}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/contact" element={<contacts/>}/>
+      <Route path="/projects" element={<projects/>}/>
+      <Route path="/about" element={<about/>}/>
+      <Route path="*" element={<notFound/>}/>
      </Routes>
     </div>
   );
